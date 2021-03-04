ce1f988484a20f20e6d61d783cb53c592f45a876
yaycarascon <79123266+yaycarascon@users.noreply.github.com>
2021-03-04 11:53:23 +0800

    header-fixed-transparent

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -73,6 +73,14 @@ $('select').change(function(){
     $('#CaseStory').show();
   } 
 })
+
+const elem = $('header');
+const scrolled = () => {
+   const threshold = $(document).scrollTop() > 50;
+   elem.toggleClass('scrolled', threshold);
+   };
+$(window).on({ scroll: scrolled });
+
   /////////////////////
   // Public Methods //
   ///////////////////
